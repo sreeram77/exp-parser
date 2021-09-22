83f41101bee9328b492a30a32d767e385426ca11
Sreeram Narayanan <sreeram.an1994@gmail.com>
2021-09-22 14:18:20 +0530

    Add actual output to Testcase model

--- model/input.go
+++ model/input.go
@@ -4,6 +4,7 @@ type TestCase struct {
 	Expression     string                 `yaml:"expression"`
 	Json           map[string]interface{} `yaml:"json"`
 	ExpectedOutput bool                   `yaml:"expected_output"`
+	ActualOutput   bool                   `yaml:"actual_output"`
 }
 
 type TestCases struct {
