288a9217b0b983d4a11e9c7fa3bad4c69378122f
Sreeram Narayanan <sreeram.an1994@gmail.com>
2021-09-24 14:58:53 +0530

    Fix write to file bug
    - marshal of yaml struct is not producing expected result

--- ioutil/io.go
+++ ioutil/io.go
@@ -1,8 +1,11 @@
 package ioutil
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 
 	"github.com/sreeram77/exp-parser/model"
 	"gopkg.in/yaml.v2"
@@ -50,10 +53,54 @@ testcases:
 }
 
 func (i ioutil) Write(t model.TestCases) error {
-	data, err := yaml.Marshal(t)
+	var result []bool
+
+	for _, v := range t.Testcase {
+		result = append(result, v.ActualOutput)
+	}
+
+	lines, err := linesFromFile(i.ipFilePath)
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile(i.opFilePath, data, 0777)
+	lines = append(lines, "")
+
+	content := ""
+	j := 0
+
+	for i, line := range lines {
+		if i > 3 && (i-1)%3 == 0 {
+			res := fmt.Sprintf("   actual_output: %s\n", strconv.FormatBool(result[j]))
+			content += res
+			j++
+		}
+		content += line
+		content += "\n"
+	}
+
+	return os.WriteFile(i.opFilePath, []byte(content), 0644)
+}
+
+func linesFromFile(path string) ([]string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return LinesFromReader(f)
+}
+
+func LinesFromReader(r io.Reader) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
 }
