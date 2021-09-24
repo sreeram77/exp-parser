f0d020d8667521c88a38b89edaedd1a34d31b40d
Sreeram Narayanan <sreeram.an1994@gmail.com>
2021-09-24 13:13:37 +0530

    Get input and output file names
    - write output to file

--- ioutil/io.go
+++ ioutil/io.go
@@ -1,18 +1,29 @@
 package ioutil
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/sreeram77/exp-parser/model"
 	"gopkg.in/yaml.v2"
 )
 
-type ioutil struct{}
+type ioutil struct {
+	ipFilePath string
+	opFilePath string
+}
 
-func New() IOUtil {
-	return ioutil{}
+func New(ip, op string) IOUtil {
+	return ioutil{ipFilePath: ip, opFilePath: op}
 }
 
 func (i ioutil) Read() (model.TestCases, error) {
-	var data = `
+	data, err := os.ReadFile(i.ipFilePath)
+	if err != nil {
+		return model.TestCases{}, err
+	}
+
+	var dat = `
 testcases:
  - expression: "$color == 'red'"
    json: {"color":"red","size":10,"cost":100.0,"mattress":{"name":"king"},"big":true,"legs":[{"length":4}]}
@@ -27,8 +38,10 @@ testcases:
    json: {"color":"red","size":10,"cost":100.0,"mattress":{"name":"king"},"big":true,"legs":[{"length":4}]}
    expected_output: false`
 
+	fmt.Println(len(dat))
+
 	var t model.TestCases
-	err := yaml.Unmarshal([]byte(data), &t)
+	err = yaml.Unmarshal([]byte(data), &t)
 	if err != nil {
 		return model.TestCases{}, err
 	}
@@ -36,6 +49,11 @@ testcases:
 	return t, nil
 }
 
-func (i ioutil) Write(model.TestCases) error {
-	panic("implment me")
+func (i ioutil) Write(t model.TestCases) error {
+	data, err := yaml.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(i.opFilePath, data, 0777)
 }
--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"os"
+
 	"github.com/sreeram77/exp-parser/ioutil"
 	"github.com/sreeram77/exp-parser/parser"
 )
 
 func main() {
 
+	inputFilePath := os.Getenv("INPUT_PATH")
+	if inputFilePath == "" {
+		inputFilePath = "./file/input.yml"
+	}
+
+	outputFilePath := os.Getenv("OUTPUT_PATH")
+	if outputFilePath == "" {
+		outputFilePath = "./file/ouput.yml"
+	}
+
 	// Initialize file reader
-	fileIO := ioutil.New()
-	yamlParser := parser.New()
+	fileIO := ioutil.New(inputFilePath, outputFilePath)
 
 	// Read file
 	t, err := fileIO.Read()
 	if err != nil {
 		panic(err)
 	}
+
+	// Initialize parser
+	yamlParser := parser.New()
 
 	// Call parser
 	processed, err := yamlParser.Parse(t)
